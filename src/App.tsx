87222f66202ce94599bdd62c9e0f4fb032843f36
KoukiIijima24 <koki.iijima@jbs.com>
2023-07-10 23:37:05 +0900

    Install TAilwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
       </header>
     </div>
   );
